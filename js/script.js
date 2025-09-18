60a651b8219ad867a74241f79407ee2d27451368
Gazi Md. Mahatab Uddin Munim <128234933+mahatabmunim@users.noreply.github.com>
2025-09-19 04:54:35 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let a=10;
+let b=2;
+let c=a+=10
+console.log(a-b)
+console.log(a+b)
+console.log(a*b)
+console.log(a/b)
+console.log(b+c)
+console.log(b-c)
+console.log(b*c)
+console.log(b/c)
+if(a>b){
+    console.log ('A gater then')
+}else{
+    console.log('B less then  ')
+}
+
+
+// var nawme ='munim'
+// var age =12
+// var age =12
+// const name = 'mahatab'
+// console.log(age)
+// console.log(name)
+
+
+//  function user(){
+//         var name = 'munim'
+//         if(true){
+//             let age=23 
+//             console.log(name)
+//             console.log(age)
+//         }
+        
+//     } 
+    
+//     user()
